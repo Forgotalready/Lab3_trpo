37d150ac272d96f08f0630193e1851fa7dbb5c69
ForgotAlready <oleg.burdakin2016@yandex.ru>
2024-08-21 18:01:54 +0700

    Написал стили для элементов

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include "View/MainWindow.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+    QFile styleF;
+
+    styleF.setFileName("D:/project/Lab3_trpo/style.qss");
+    styleF.open(QFile::ReadOnly);
+    QString qssStr = styleF.readAll();
+
+    qApp->setStyleSheet(qssStr);
+
     MainWindow w;
     w.show();
     return a.exec();
